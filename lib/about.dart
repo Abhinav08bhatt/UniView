bf3cb13734cca9fbc66a13f42f55bf06385b5b06
Abhinav08bhatt <abhinav08bhatt@gmail.com>
2026-01-24 20:23:06 +0530

    added about page file

---
+++ lib/about.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:uniview/graphic_era_theme.dart';
+// import 'package:url_launcher/url_launcher.dart';
+
+class AboutPage extends StatelessWidget {
+  const AboutPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: () {
+          FocusManager.instance.primaryFocus?.unfocus();
+        },
+
+/*  ----------------------------------------- MAIN CLASS ----------------------------------------- */
+        child: const _AboutPage(),
+      ),
+    );
+  }
+}
+
+class _AboutPage extends StatelessWidget {
+  const _AboutPage();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+
+/*  --------------------------------------- PAGE PROPERTIES --------------------------------------- */
+      physics: const BouncingScrollPhysics(),
+      keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+      padding: EdgeInsets.zero,
+
+      children: const [
+      ]
+    );
+    
+  }
+}
